c5c2b72156d8041e23db47b19e2adadb71e0bdd4
Ruben Vasquez <neburtnt@gmail.com>
2025-11-18 11:49:42 -0500

    Anticlick

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
 import './App.css'
+import { useEffect } from "react";
 import { AppRoutes } from './setup/routes/AppRoutes'
 import { useOfflineCache } from './hooks/useOfflineCache'
 import { OfflineIndicator } from './components/OfflineIndicator'
 import { PWAUpdateHandler } from './components/PWAUpdateHandler'
 
 function App() {
-  useOfflineCache()
+  useOfflineCache();
+
+  useEffect(() => {
+    // Bloquear menú contextual (anticlick)
+    const handleContextMenu = (e) => e.preventDefault();
+
+    // Bloquear long-press en móviles
+    const blockLongPress = (e) => {
+      if (e.touches.length === 1) {
+        // Opción suave: evita long press sin afectar scroll
+        if (e.target.tagName !== "INPUT" && e.target.tagName !== "TEXTAREA") {
+          e.preventDefault();
+        }
+      }
+    };
+
+    document.addEventListener("contextmenu", handleContextMenu);
+    document.addEventListener("touchstart", blockLongPress, { passive: false });
+
+    return () => {
+      document.removeEventListener("contextmenu", handleContextMenu);
+      document.removeEventListener("touchstart", blockLongPress);
+    };
+  }, []);
+
   return (
     <>
       <AppRoutes />
@@ -15,4 +40,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
